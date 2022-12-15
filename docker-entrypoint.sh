a74320e51879a691d0d8f700c65f075fae9b9eab
Nathan Lee <113044550+nathandotleeathpe@users.noreply.github.com>
2022-12-15 14:08:06 -0600

    Allowing compute nodes to oversubscribe CPU/Mem

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -28,12 +28,12 @@ then
 
     echo "---> Waiting for slurmdbd to become active before starting slurmctld ..."
 
-    until 2>/dev/null >/dev/tcp/slurmdbd/6819
-    do
-        echo "-- slurmdbd is not available.  Sleeping ..."
-        sleep 2
-    done
-    echo "-- slurmdbd is now active ..."
+    # until 2>/dev/null >/dev/tcp/slurmdbd/6819
+    # do
+    #     echo "-- slurmdbd is not available.  Sleeping ..."
+    #     sleep 2
+    # done
+    # echo "-- slurmdbd is now active ..."
 
     echo "---> Starting the Slurm Controller Daemon (slurmctld) ..."
     if /usr/sbin/slurmctld -V | grep -q '17.02' ; then
